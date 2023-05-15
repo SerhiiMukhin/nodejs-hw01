9c0820a2c61722d3ee71d959e3e85869248e05cd
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-05-15 20:36:40 +0300

    CLI contacts app

---
+++ index.js
@@ -0,0 +1,39 @@
+const { program } = require('commander');
+
+const contacts = require('./contacts');
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+
+    case 'get':
+      const contact = await contacts.getContactById(id);
+      return console.table(contact);
+
+    case 'add':
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.table(newContact);
+
+    case 'remove':
+      const deleteById = await contacts.removeContact(id);
+      return console.table(deleteById);
+
+    default:
+      console.log('Unknown action');
+  }
+};
+
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+invokeAction(argv);
